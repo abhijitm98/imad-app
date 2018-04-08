97edcfaa37539520f816cc133b3c10760f5894a7
abhijitm98 <37284711+abhijitm98@users.noreply.github.com>
2018-04-09 02:27:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,6 @@ element.innerHTML=' new value of course';
 var element= document.getElementById('madi');
 function moveright()
 {
-  img.style.marginLeft=marginLeft+100;
+  element.style.marginLeft=marginLeft+100;
 }
 element.onclick(moveright());
