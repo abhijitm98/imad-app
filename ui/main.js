82f6f3f4987306335d2eb05092566da3305a7b11
abhijitm98 <37284711+abhijitm98@users.noreply.github.com>
2018-04-09 02:38:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,12 @@ console.log('Loaded!');
 var element= document.getElementById('main');
 element.innerHTML=' new value of course';
 var element= document.getElementById('madi');
+function moveright()
+{
+    marginLeft=marginLeft+10;
+    element.style.marginLeft=marginLeft+'px';
+}
 
-
-element.onclick=function moveright()
-{var marginLeft=0;
-  element.style.marginLeft='100px';
+element.onclick=function ()
+{var interval= setInterval(moveright(),100);
 };
