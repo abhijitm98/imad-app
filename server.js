41f39cc326261943447a4bb76cf033d76fe42dcf
abhijitm98 <37284711+abhijitm98@users.noreply.github.com>
2018-04-08 20:26:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.get('/article1', function (req, res)
 app.get('/article2', function (req, res) 
                     {
                         res.sendFile(path.join(__dirname, 'ui','article2.html'));
+                        console.log('Here is your article2');
                     }
         );
 
