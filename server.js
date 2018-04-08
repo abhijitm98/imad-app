0b36eb9da3921035a4964b02ccfb1777bebb0b80
abhijitm98 <37284711+abhijitm98@users.noreply.github.com>
2018-04-08 20:22:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   });
 
-app.get('/article1', function (req, res) {
-  res.send('Article is reqiested and will be served');
-});
+app.get('/article1', function (req, res) 
+                    {
+                        res.send('Article is reqiested and will be served');
+                    }
+        );
+app.get('/article2', function (req, res) 
+                    {
+                        res.sendFile(path.join(_dirname,'ui','article2.html'));
+                    }
+        );
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
