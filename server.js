cb14230fdb7d77b90668bed40cfcfccb39cad7f1
abhijitm98 <37284711+abhijitm98@users.noreply.github.com>
2018-04-25 11:55:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get('/test-db',function(req,res){
         {
             res.status(500).send(err.toString());
         }else {
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.rows));
         }
     });
     //get a response from the database and display
