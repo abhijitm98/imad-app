3ae5115c64a87e059857f25f6b65817dd38cfabc
abhijitm98 <37284711+abhijitm98@users.noreply.github.com>
2018-04-27 12:22:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,14 @@
 
 var button=document.getElementById('submit');
 button.onclick=function(){
+    alert('Button is responding');
     //create a requets object
     var username=document.getElementById('username').value;
     var password=document.getElementById('password').value;  
     
     var request= new XMLHttpRequest();
     request.onreadystatechange=check;
-    request.open('POST','http://abhijitmajee1.imad.hasura-app.io/login-user',true);
+    request.open('POST','http://abhijitmajee1.imad.hasura-app.io/login',true);
     request.setRequestHeader('content-type','application/json');
     request.send(JSON.stringify({username:username,password:password}));
     
